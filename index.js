e5a9775d3adbbc53c360d45690fab8fd55e85801
Gouki.ET <scubillosq@gmail.com>
2022-11-14 16:52:40 -0300

    add heroku port config

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json())
 
 app.use('/api', router)
-const port = 4000;
-app.listen(port, ()=> console.log('Server listening on port ' + port))
+
+const port = process.env.PORT || 4000;
+app.listen(port, '0.0.0.0', ()=> console.log('Server listening on port ' + port))
 /* app.listen(4000, ()=>{console.log('Server listening on port 4000')}) */
